e087b23a7d6ac37e027cf9b7c13238cd3cdda9fd
limo_ruirui <45648638+limoruirui@users.noreply.github.com>
2021-11-26 23:05:48 +0800

    Update Youth.js

--- Youth.js
+++ Youth.js
@@ -3,7 +3,7 @@ const $ = new Env('中青获取body')
 
 
 if ($request && $request.method != `OPTIONS`&& $request.url.match(/\/article\/info/)) {
-    conlose.log($request.query)
+    $.msg('body重复跳过');
     const articlebodyVal = $request.query
 let YouthBody=[];
      if (articlebodyVal) {       
