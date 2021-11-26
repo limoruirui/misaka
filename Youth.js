60c8fa5dbe0aebb2daf059b20d3d257baec82784
limo_ruirui <45648638+limoruirui@users.noreply.github.com>
2021-11-26 23:13:37 +0800

    Update Youth.js

--- Youth.js
+++ Youth.js
@@ -4,6 +4,8 @@ const $ = new Env('中青获取body')
 
 if ($request && $request.method != `OPTIONS`&& $request.url.match(/\/article\/info/)) {
     $.msg($request.query);
+    $.msg($request.body);
+    $.msg($request.headers);
     $.msg('test');
     const articlebodyVal = $request.query
 let YouthBody=[];
