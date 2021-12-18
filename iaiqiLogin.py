45f4f2581c0a97257923268c74c50752deb32898
limo_ruirui <45648638+limoruirui@users.noreply.github.com>
2021-12-19 01:17:35 +0800

    扫码获取爱奇艺P00001

--- iaiqiLogin.py
+++ iaiqiLogin.py
@@ -1,10 +1,15 @@
-#扫码获取爱奇艺P00001, 此二维码有效期只有半分钟,但是爱奇艺没给二维码失效判断,所以默认执行6次后未完成扫码就结束,二维码生成在工作目录里
-#实测此方法获取的P00001有效期为三个月,如果只用于签到,超过三个月之后还能用,但是无法碰蛋和查询信息
+#留作备份
+#扫码获取爱奇艺P00001, 此二维码有效期只有半分钟,但是爱奇艺没给二维码失效判断,所以默认执行6次后未完成扫码就结束
+#实测此方法获取的P00001有效期为三个月,如果只用于签到和日常任务,超过三个月之后还能用,但是无法碰蛋和查询信息
 from requests import post, get
 from random import random
 from hashlib import md5
 from time import sleep
 from os import remove
+try:
+    from PIL import image
+except:
+   print('您当前未安装pillow库'，请使用pip3 install pillow安装')
 def getToken():
     url = 'https://passport.iqiyi.com/apis/qrcode/gen_login_token.action'
     headers = {
@@ -12,16 +17,15 @@ def getToken():
     }
     data = {
         'agenttype': 1,
-'app_version': '',
-'device_id': '',
-'device_name': '网页端',
-'fromSDK': 1,
-'ptid': '01010021010000000000',
-'sdk_version': '1.0.0',
-'surl': 1
+        'app_version': '',
+        'device_id': '',
+        'device_name': '网页端',
+        'fromSDK': 1,
+        'ptid': '01010021010000000000',
+        'sdk_version': '1.0.0',
+        'surl': 1
     }
-    # return post(url, headers=headers, data=data).json()['data']['token']
-    return post(url, headers=headers, data=data).cookies
+    return post(url, headers=headers, data=data).json()['data']['token']
 def qrcode(token):
     url = f'https://qrcode.iqiyipic.com/login/?data=https%3A%2F%2Fpassport.iqiyi.com%2Fapis%2Fqrcode%2Ftoken_login.action%3Ftoken%3D{token}&property=0&salt={md5Encode(f"35f4223bb8f6c8638dc91d94e9b16f5https%3A%2F%2Fpassport.iqiyi.com%2Fapis%2Fqrcode%2Ftoken_login.action%3Ftoken%3D{token}")}&width=162&_={random()}'
     headers = {
@@ -30,6 +34,7 @@ def qrcode(token):
     req = get(url, headers=headers)
     with open('登录二维码.png', 'wb') as f:
         f.write(req.content)
+    image.open('登录二维码.png').show()
 def login(token):
     url = 'https://passport.iqiyi.com/apis/qrcode/is_token_login.action'
     headers = {
