6a747a14593cd6e31b5cedf5296e16240d50147b
limo_ruirui <45648638+limoruirui@users.noreply.github.com>
2022-05-06 19:05:09 +0800

    爱奇艺每日任务

--- iqiyi.py
+++ iqiyi.py
@@ -5,27 +5,33 @@
 # @Time : 5/5/2022 20:09
 # -------------------------------
 """
-1.爱奇艺每日任务脚本 请低调使用 请不要用于商业牟利
+1.爱奇艺每日任务脚本 请低调使用 请不要用于商业牟利 一天一次 请自行斟酌设置crontab
 2.cookie获取方式
     1.cookie可以用别人loon、qx等软件的mitm类自动获取再去boxjs里复制出来填写到环境变量或本脚本中
     2.也可以自行抓包 电脑或者手机都可以
     3.实在都不会
 3.cookie食用方式: 可以只保留P00001=xxx;中xxx的值 也可以整段都要 青龙运行可新建并放入到环境变量 iqy_ck 中 也可以直接填写在本脚本中
-4.库中有每月自动领取爱奇艺会员天数红包的脚本 可配合使用(需有高等级的运行脚本提供红包 其它人才可以领取)
+4.关于dfp
+    1.dfp相当于爱奇艺的浏览器指纹 不需要登录也会有 有效期非常长 实测半年多前的还能用 其中领取每日任务的奖励和刷观影时长都需要使用到
+    2.dfp目前本脚本写死了一个 但是多用户使用同一个不知道有没有风险
+    3.建议有能力的自己抓包 在cookie里的__dfp字段 然后环境变量新增 iqiyi_dfp 填入 或者在本脚本内写死
+    4.不会自己抓的话 请打开设置环境变量 get_iqiyi_dfp 为 True 再执行脚本会获得并输出到面板 请复制后按上一条填入环境变量 获取完请删除get_iqiyi_dfp环境变量 小鸡经不起操
+    5.get请求 没携带任何东西出去 开源脚本 请不要说什么提交什么东西到我服务器
+5.库中有每月自动领取爱奇艺会员天数红包的脚本 可配合使用(需有高等级的运行脚本提供红包 其它人才可以领取)
 """
 cookie = ""
+iqiyi_dfp = ""
 from time import sleep, time
 from random import randint, choice
 from json import dumps, loads
 from hashlib import md5 as md5Encode
 from string import digits, ascii_lowercase, ascii_uppercase
-from sys import exit
+from sys import exit, stdout
 from os import environ, system
-from copy import deepcopy
 from re import findall
 
 try:
-    from requests import Session
+    from requests import Session, get, post
     from fake_useragent import UserAgent
 except:
     print(
@@ -35,23 +41,36 @@ except:
     print("安装完成 脚本退出 请重新执行")
     exit(0)
 iqy_ck = environ.get("iqy_ck") if environ.get("iqy_ck") else cookie
+get_iqiyi_dfp = environ.get("get_iqiyi_dfp") if environ.get("get_iqiyi_dfp") else False
+pushplus_token = environ.get("PUSH_PLUS_TOKEN") if environ.get("PUSH_PLUS_TOKEN") else ""
+tgbot_token = environ.get("TG_BOT_TOKEN") if environ.get("TG_BOT_TOKEN") else ""
+tg_userId = environ.get("TG_USER_ID") if environ.get("TG_USER_ID") else ""
+tg_push_api = environ.get("TG_API_HOST") if environ.get("TG_API_HOST") else ""
 if iqy_ck == "":
     print("未填写cookie 青龙可在环境变量设置 iqy_ck 或者在本脚本文件上方将获取到的cookie填入cookie中")
     exit(0)
+if "__dfp" in iqy_ck:
+    iqiyi_dfp = findall(r"__dfp=(.*?)(;|$)", iqy_ck)[0][0]
+    iqiyi_dfp = iqiyi_dfp.split("@")[0]
 if "P00001" in iqy_ck:
     iqy_ck = findall(r"P00001=(.*?)(;|$)", iqy_ck)[0][0]
+if iqiyi_dfp == "":
+    iqiyi_dfp = environ.get("iqiyi_dfp") if environ.get(
+        "iqiyi_dfp") else "a18af56a9b6a224272ab8ed00d1a587078cd5c8ab119b2a4a689d5a22f06bcbd8b"
 
 
 class Iqiyi:
-    def __init__(self, ck):
+    def __init__(self, ck, dfp):
         self.ck = ck
         self.session = Session()
+        self.user_agent = UserAgent().chrome
         self.headers = {
-            "User-Agent": UserAgent().chrome,
+            "User-Agent": self.user_agent,
             "Cookie": f"P00001={self.ck}",
             "Content-Type": "application/json"
         }
         self.uid = ""
+        self.dfp = dfp
         self.msg = ""
         self.user_info = ""
         self.sleep_await = environ.get("sleep_await") if environ.get("sleep_await") else 1
@@ -64,23 +83,23 @@ class Iqiyi:
             try:
                 data = self.session.get(url, headers=self.headers, params=body).json()
             except:
-                print("请求发送失败,可能为网络异常")
+                self.print_now("请求发送失败,可能为网络异常")
             #     data = self.session.get(url, headers=self.headers, params=body).text
             return data
         elif req_method.upper() == "POST":
             try:
                 data = self.session.post(url, headers=self.headers, data=dumps(body)).json()
             except:
-                print("请求发送失败,可能为网络异常")
+                self.print_now("请求发送失败,可能为网络异常")
             #     data = self.session.post(url, headers=self.headers, data=dumps(body)).text
             return data
         elif req_method.upper() == "OTHER":
             try:
                 self.session.get(url, headers=self.headers, params=dumps(body))
             except:
-                print("请求发送失败,可能为网络异常")
+                self.print_now("请求发送失败,可能为网络异常")
         else:
-            print("您当前使用的请求方式有误,请检查")
+            self.print_now("您当前使用的请求方式有误,请检查")
 
     def timestamp(self, short=False):
         if (short):
@@ -101,13 +120,75 @@ class Iqiyi:
                 str += choice(digits + ascii_lowercase)
         return str
 
+    def pushplus(self, title, content):
+        url = "http://www.pushplus.plus/send"
+        headers = {
+            "Content-Type": "application/json"
+        }
+        data = {
+            "token": pushplus_token,
+            "title": title,
+            "content": content
+        }
+        try:
+            post(url, headers=headers, data=dumps(data))
+        except:
+            self.print_now('推送失败')
+
+    def tgpush(self, content):
+        url = f"https://api.telegram.org/bot{tgbot_token}/sendMessage"
+        if tg_push_api != "":
+            url = f"https://{tg_push_api}/bot{tgbot_token}/sendMessage"
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        data = {'chat_id': str(tg_userId), 'text': content, 'disable_web_page_preview': 'true'}
+        try:
+            post(url, headers=headers, data=data, timeout=10)
+        except:
+            self.print_now('推送失败')
+
+    def print_now(self, content):
+        print(content)
+        stdout.flush()
+
+    def get_dfp_params(self):
+        get_params_url = "https://iqiyi.ruirui.fun/api/iqiyi/get_dfp"
+        data = get(get_params_url).json()
+        return data
+
+    def get_dfp(self):
+        body = self.get_dfp_params()
+        url = "https://cook.iqiyi.com/security/dfp_pcw/sign"
+        headers = {
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "zh-CN,zh;q=0.9",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "Content-Length": "1059",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Host": "cook.iqiyi.com",
+            "Origin": "https://www.iqiyi.com",
+            "Pragma": "no-cache",
+            "Referer": "https://www.iqiyi.com/",
+            "sec-ch-ua": f"\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"{body['data']['sv']}\", \"Google Chrome\";v=\"{body['data']['sv']}\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "Windows",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "User-Agent": self.user_agent
+        }
+        data = post(url, headers=headers, data=body["data"]["body"]).json()
+        self.dfp = data["result"]["dfp"]
+        self.print_now(self.dfp)
+
     def get_userinfo(self):
         url = f"https://tc.vip.iqiyi.com/growthAgency/v2/growth-aggregation?messageId={self.qyid}&platform=97ae2982356f69d8&P00001={self.ck}&responseNodes=duration%2Cgrowth%2Cupgrade%2CviewTime%2CgrowthAnnualCard&_={self.timestamp()}"
         data = self.req(url)
         msg = data['data']['growth']
         try:
-            self.user_info = f"查询成功: 到期时间{msg['deadline']}\t当前等级为{msg['level']}\n\t\t今日获得成长值{msg['todayGrowthValue']}\t总成长值{msg['growthvalue']}\t距离下一等级还差{msg['distance']}成长值"
-            print(self.user_info)
+            self.user_info = f"查询成功: 到期时间{msg['deadline']}\t当前等级为{msg['level']}\n\t今日获得成长值{msg['todayGrowthValue']}\t总成长值{msg['growthvalue']}\t距离下一等级还差{msg['distance']}成长值"
+            self.print_now(self.user_info)
         except:
             self.user_info = f"查询失败,未获取到用户信息"
 
@@ -119,14 +200,20 @@ class Iqiyi:
         if data.get("code") == 'A00000':
             self.uid = data['data']['userinfo']['pru']
         else:
-            print("请求api失败 可能是网络问题 也可能接口被玩坏了")
+            self.print_now("请求api失败 可能是网络问题 也可能接口被玩坏了")
             exit(0)
+
+    def get_watch_time(self):
+        url = "https://tc.vip.iqiyi.com/growthAgency/watch-film-duration"
+        data = self.req(url)
+        watch_time = data['data']['viewtime']['time']
+        return watch_time
 
     def get_sign(self):
         self.qyid = self.md5(self.uuid(16))
         time_stamp = self.timestamp()
         if self.uid == "":
-            print("获取用户id失败 可能为cookie设置错误或者网络异常,请重试或者检查cookie")
+            self.print_now("获取用户id失败 可能为cookie设置错误或者网络异常,请重试或者检查cookie")
             exit(0)
         data = f'agentType=1|agentversion=1|appKey=basic_pcw|authCookie={self.ck}|qyid={self.qyid}|task_code=natural_month_sign|timestamp={time_stamp}|typeCode=point|userId={self.uid}|UKobMjDMsDoScuWOfp6F'
         url = f'https://community.iqiyi.com/openApi/task/execute?agentType=1&agentversion=1&appKey=basic_pcw&authCookie={self.ck}&qyid={self.qyid}&sign={self.md5(data)}&task_code=natural_month_sign&timestamp={time_stamp}&typeCode=point&userId={self.uid}'
@@ -150,9 +237,9 @@ class Iqiyi:
         data = self.req(url, "post", body)
         # print(data)
         if data.get('code') == 'A00000':
-            print(f"签到执行成功, {data['data']['msg']}")
+            self.print_now(f"签到执行成功, {data['data']['msg']}")
         else:
-            print("签到失败，原因可能是签到接口又又又又改了")
+            self.print_now("签到失败，原因可能是签到接口又又又又改了")
 
     def dailyTask(self):
         taskcodeList = {'b6e688905d4e7184': "浏览生活福利",
@@ -171,9 +258,12 @@ class Iqiyi:
                 # print(f'完成{taskcodeList[taskcode]}任务成功')
                 sleep(2)
             # 领取奖励
-            url = f'https://tc.vip.iqiyi.com/taskCenter/task/getTaskRewards?P00001={self.ck}&taskCode={taskcode}&dfp=a168db20cec470441bac40a7052e573a18cf2190f6c78a1e946cc17450c9fc9b15&platform=b6c13e26323c537d&lang=zh_CN&app_lm=cn&deviceID={self.md5(self.uuid(8))}&token=&multiReward=1&fv=bed99b2cf5722bfe'
-            price = self.req(url)['dataNew'][0]["value"]
-            print(f"领取{taskcodeList[taskcode]}任务奖励成功, 获得{price}点成长值")
+            url = f'https://tc.vip.iqiyi.com/taskCenter/task/getTaskRewards?P00001={self.ck}&taskCode={taskcode}&dfp={self.dfp}&platform=b6c13e26323c537d&lang=zh_CN&app_lm=cn&deviceID={self.md5(self.uuid(8))}&token=&multiReward=1&fv=bed99b2cf5722bfe'
+            try:
+                price = self.req(url)['dataNew'][0]["value"]
+                self.print_now(f"领取{taskcodeList[taskcode]}任务奖励成功, 获得{price}点成长值")
+            except:
+                self.print_now(f"领取{taskcodeList[taskcode]}任务奖励可能出错了 也可能没出错 只是你今天跑了第二次")
             sleep(5)
 
     def lottery_draw(self, lottery=True):
@@ -199,32 +289,41 @@ class Iqiyi:
             "secure_p": "0",
             "req_sn": self.timestamp()
         }
-        get_change_params = deepcopy(lottery_params)
-        get_change_params["lottery_chance"] = 1
-        params = get_change_params
-        if lottery:
-            params = lottery_params
+        # get_change_params = deepcopy(lottery_params)
+        # get_change_params["lottery_chance"] = 1
+        # params = get_change_params
+        # if lottery:
+        #     params = lottery_params
+        params = lottery_params
         data = self.req(url, "get", params)
-        if lottery and data.get("code") == 0:
-            print(data["kv"]["msg"])
+        # if lottery and data.get("code") == 0:
+        if data.get("code") == 0:
+            self.print_now(f'抽奖成功, 获得{data["awardName"]}')
             return data["daysurpluschance"]
-        elif not lottery and data.get("code") == 0:
-            return data["daysurpluschance"]
+        # elif not lottery and data.get("code") == 0:
+        #     return data["daysurpluschance"]
 
     def start(self):
-        print("正在执行刷观影时长脚本 为减少风控 本过程运行时间较长 大概半个小时")
-        totalTime = 0
+        self.print_now("正在执行刷观影时长脚本 为减少风控 本过程运行时间较长 大概半个小时")
+        totalTime = self.get_watch_time()
+        if totalTime >= 7200:
+            self.print_now(f"你的账号今日观影时长大于2小时 不执行刷观影时长")
+            return
         for i in range(150):
             Time = randint(60, 120)
-            dfp = self.uuid(66)
-            url = self.getUrl(Time, dfp)
+            # dfp = self.uuid(66)
+            url = self.getUrl(Time, self.dfp)
             self.req(url, 'other')
             totalTime += Time
             sleep(randint(20, 40))
+            if i % 20 == 3:
+                self.print_now(f"现在已经刷到了{totalTime}秒, 数据同步有延迟, 仅供参考")
             if totalTime >= 7600:
                 break
 
     def main(self):
+        if get_iqiyi_dfp:
+            self.get_dfp()
         self.getUid()
         self.get_sign()
         self.start()
@@ -235,12 +334,16 @@ class Iqiyi:
             sleep(3)
         self.sign()
         self.dailyTask()
-        print(f"任务已经执行完成, 因爱奇艺观影时间同步较慢,这里等待3分钟再查询今日成长值信息,若不需要等待直接查询,请设置环境变量名 sleep_await = 0 默认为等待")
+        self.print_now(f"任务已经执行完成, 因爱奇艺观影时间同步较慢,这里等待3分钟再查询今日成长值信息,若不需要等待直接查询,请设置环境变量名 sleep_await = 0 默认为等待")
         if int(self.sleep_await) == 1:
             sleep(180)
         self.get_userinfo()
+        if pushplus_token != "":
+            self.pushplus(self.user_info)
+        if tgbot_token != "" and tg_userId != "":
+            self.tgpush(self.user_info)
 
 
 if __name__ == '__main__':
-    iqiyi = Iqiyi(iqy_ck)
+    iqiyi = Iqiyi(iqy_ck, iqiyi_dfp)
     iqiyi.main()
