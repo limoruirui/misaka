d000c9cfeb060b74611ada271a9b385da2220454
院长 <32212075+yuanter@users.noreply.github.com>
2023-09-15 22:02:17 +0800

    沃畅游短信登录,修复2.11.0-2.12.0之间的青龙版本问题

--- wochangyou_token.py
+++ wochangyou_token.py
@@ -394,13 +394,16 @@ def login_post(cookieKey):
                     if ck_temp["remarks"] == phone:
                         flag_temp = True
                         if flag == "old":
+                            _id = ck_temp.get("_id",None)
+                            if not _id:
+                                _id = ck_temp["id"]
+                            put_envs(_id, ck_temp['name'], access_token, phone)
                             # print("进入旧版本青龙禁用方法")
-                            put_envs(ck_temp["_id"], ck_temp['name'], access_token, phone)
-                            # disable_env(ck_temp["_id"])
-                            # delete_env(ck_temp["_id"])
+                            # disable_env(_id)
+                            # delete_env(_id)
                         elif flag == "new":
-                            # print("进入新版本青龙禁用方法")
                             put_envs(ck_temp["id"], ck_temp['name'], access_token, phone)
+                            # print("进入新版本青龙禁用方法")
                             # disable_env(ck_temp["id"])
                             # delete_env(ck_temp["id"])
                         print_now(f"账号【{phone}】自动更新access_token至青龙环境：WoChangYouCK  备注为：{phone}")
